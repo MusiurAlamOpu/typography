5e5d738f8101c02fe0441f267b1c044fd53d9af0
Musiur Alam Opu <musiuralamo@gmail.com>
2021-02-17 02:24:37 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const itemElement = document.getElementById('body');
+itemElement.addEventListener('click', function(){
+    
+    itemElement.innerHTML = `
+    <p style="font-size: 70px; text-align: center;">Allahu Akbar</p>
+    `;
+})

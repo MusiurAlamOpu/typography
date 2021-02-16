202ba27cc82c99cf69d159bb5588f39bc52921ff
Musiur Alam Opu <musiuralamo@gmail.com>
2021-02-17 04:54:32 +0600

    after completing home sectino

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-const itemElement = document.getElementById('body');
-itemElement.addEventListener('click', function(){
-    
-    itemElement.innerHTML = `
-    <p style="font-size: 70px; text-align: center;">Allahu Akbar</p>
-    `;
+
+//data field
+const homeClicked = document.getElementById("homeClicked");
+const categoryClicked = document.getElementById("categoryClicked");
+const topClicked = document.getElementById("topClicked");
+const logInClicked = document.getElementById("logInClicked");
+const signUpClicked = document.getElementById("signUpClicked");
+const aboutClicked = document.getElementById("aboutClicked");
+const mainDiv = document.getElementById("mainDiv");
+const homeSection = document.getElementById("homeSection");
+const logInSection = document.getElementById("logInSection");
+
+
+//event handling
+logInClicked.addEventListener("click", () => {
+    let sectionView1 = homeSection;
+    sectionView1.style.display = 'none';
+})
+homeSection.addEventListener("click", () => {
+    mainDiv.innerHTML = homeSection.innerHTML;
 })
